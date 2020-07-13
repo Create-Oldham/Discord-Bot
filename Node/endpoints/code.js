4a9efb55a9f91e2cfd40aad2e3e7bb1eecf8470d
keiranwilkinson <keiran@keiranwilkinson.com>
2020-07-13 14:59:48 +0100

    Fixing error where the code woudl crash if someone didn't have permission to access the code for a certain machine.

--- Node/endpoints/code.js
+++ Node/endpoints/code.js
@@ -21,13 +21,12 @@ module.exports = {
 
                         command.RunQuery()
                             .then((result) => {
-                                console.log(result.recordsets[1])
+                                console.log(result)
                                 var resultOutcome = result.recordset[0].result;
-                                console.log(result.recordsets[1][0].EquipmentName)
                                 if (resultOutcome.toLowerCase() === "success") {
                                     message.author.send("Instructions for the *" + result.recordsets[1][0].EquipmentName + "* \n" + result.recordsets[1][0].EquipmentInstructions)
                                 } else if (resultOutcome.toLowerCase() === "failiure") {
-                                    message.author.send("You don't have permission to view the code for this machine")
+                                    message.author.send("You don't have permission to view the code for the machine *" + searchTerm +"*")
                                     console.log(message.author.id);
                                 } else if (resultOutcome.toLowerCase() === "invalid") {
                                     message.author.send("The search term " + searchTerm + " returned no results, use !Equipment to get a list of potential equipment names ")
