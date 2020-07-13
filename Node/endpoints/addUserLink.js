6fc7a3d944555c0f5fbfd7ff687080197ebcea7f
Keiran Wilkinson <keiran@keiranwilkinson.com>
2020-07-13 14:19:18 +0100

    Added validation for blank search term

--- Node/endpoints/addUserLink.js
+++ Node/endpoints/addUserLink.js
@@ -11,49 +11,54 @@ module.exports = {
             }
 
             var searchTerm = searchTermRaw.trim();
-            const args = splitMessage[1];
-            if (args) {
-                const user = require("../utils/discordUtils.js").getUserFromMention(client, args);
-                if (!user) {
-                    return message.reply('Please use a proper mention if you want to add someone to a piece of equipment');
-                } else {
-                    sql.connect()
-                        .then((conn) => {
-                            new sql.command('EquipmentLink_ins', conn)
-                                .then((command) => {
-                                    command.input('SearchTerm', sql.sqlType.VarChar(50), searchTerm);
-                                    command.input('DiscordID', sql.sqlType.VarChar(25), user.id);
-                                    command.input('Admin', sql.sqlType.TinyInt, 0);
-                                    command.input('UID', sql.sqlType.VarChar(25), message.member.id);
+            if (searchTerm !== '') {
 
-                                    command.RunQuery()
-                                        .then((result) => {
-                                            var resultOutcome = result.recordset[0].result;
-                                            console.log(resultOutcome)
-                                            if (resultOutcome.toLowerCase() === "success") {
-                                                message.reply("Added " + '<@' + user.id + '>' + " to the machine ")
-                                            } else if (resultOutcome.toLowerCase() === "exists") {
-                                                message.reply('<@' + user.id + '>' + " already exists with a link to " + searchTerm);
+                const args = splitMessage[1];
+                if (args) {
+                    const user = require("../utils/discordUtils.js").getUserFromMention(client, args);
+                    if (!user) {
+                        return message.reply('Please use a proper mention if you want to add someone to a piece of equipment');
+                    } else {
+                        sql.connect()
+                            .then((conn) => {
+                                new sql.command('EquipmentLink_ins', conn)
+                                    .then((command) => {
+                                        command.input('SearchTerm', sql.sqlType.VarChar(50), searchTerm);
+                                        command.input('DiscordID', sql.sqlType.VarChar(25), user.id);
+                                        command.input('Admin', sql.sqlType.TinyInt, 0);
+                                        command.input('UID', sql.sqlType.VarChar(25), message.member.id);
 
-                                            } else if (resultOutcome.toLowerCase() === "failiure") {
-                                                message.reply("Permissions Issue")
-                                                console.log(message.member.id);
-                                            } else if (resultOutcome.toLowerCase() === "invalid") {
-                                                message.reply("The search term" + searchTerm + " returned no results, use !Equipment to get a list of potential equipment names")
-                                                console.log(message.member.id);
-                                            } else {
-                                                message.reply("Failiure due to unknown reason")
-                                            }
-                                        })
-                                        .catch((err) => sql.responseError(err, 'run query'));
-                                })
-                                .catch((err) => sql.responseError(err, 'command'));
-                        })
-                        .catch((err) => sql.responseError(err, 'connection'));
+                                        command.RunQuery()
+                                            .then((result) => {
+                                                var resultOutcome = result.recordset[0].result;
+                                                console.log(resultOutcome)
+                                                if (resultOutcome.toLowerCase() === "success") {
+                                                    message.reply("Added " + '<@' + user.id + '>' + " to the machine ")
+                                                } else if (resultOutcome.toLowerCase() === "exists") {
+                                                    message.reply('<@' + user.id + '>' + " already exists with a link to " + searchTerm);
+
+                                                } else if (resultOutcome.toLowerCase() === "failiure") {
+                                                    message.reply("Permissions Issue")
+                                                    console.log(message.member.id);
+                                                } else if (resultOutcome.toLowerCase() === "invalid") {
+                                                    message.reply("The search term" + searchTerm + " returned no results, use !Equipment to get a list of potential equipment names")
+                                                    console.log(message.member.id);
+                                                } else {
+                                                    message.reply("Failiure due to unknown reason")
+                                                }
+                                            })
+                                            .catch((err) => sql.responseError(err, 'run query'));
+                                    })
+                                    .catch((err) => sql.responseError(err, 'command'));
+                            })
+                            .catch((err) => sql.responseError(err, 'connection'));
+                    }
                 }
+            } else {
+                message.reply("You don't have a role that allows adding induction privledges")
             }
         } else {
-        message.reply("You don't have a role that allows adding induction privledges")
-    }
+            message.reply("Please enter a search term")
+        }
     }
 }
